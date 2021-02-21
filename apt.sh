67810eba1c6280a8513f631205e312c082677ca9
Mateusz Siwiec <mat.siwiec@gmail.com>
2021-02-21 16:39:04 +0000

    gitignore added

--- apt.sh
+++ apt.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/bash
-
 sudo apt-get update
 sudo apt-get -y dist-upgrade
 sudo apt-get -y install \
--- install.sh
+++ install.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/bash
-
 for f in $(ls dotfiles); do
     ln -svf "$PWD/dotfiles/$f" "$HOME/.$f"
 done
